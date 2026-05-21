f03ebf41d276b477e718e12c222d6989093fd691
Nikola1201 <160769567+Nikola1201@users.noreply.github.com>
2026-05-22 01:10:26 +0200

    feat(domain): dodata InstrKat entitetska klasa za vezu Instruktor x Kategorija

---
+++ Common/Domain/InstrKat.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+
+namespace Common.Domain
+{
+    [Serializable]
+    public class InstrKat : IEntity
+    {
+        public int InstruktorId { get; set; }
+        public int KategorijaID { get; set; }
+        public DateTime DatumDodele { get; set; }
+        public bool Aktivno { get; set; }
+
+        public string TableName => "InstrKat";
+
+        public string Values =>
+            $"{InstruktorId}, {KategorijaID}, '{DatumDodele:yyyy-MM-dd HH:mm:ss}', {(Aktivno ? 1 : 0)}";
+
+        public string Query => $"InstruktorId = {InstruktorId}";
+
+        public string TableKeyColumn => "InstruktorId, KategorijaID";
+
+        public string TableKeyQuery =>
+            $"InstruktorId = {InstruktorId} AND KategorijaID = {KategorijaID}";
+
+        public string Update =>
+            $"UPDATE InstrKat SET " +
+            $"DatumDodele = '{DatumDodele:yyyy-MM-dd HH:mm:ss}', " +
+            $"Aktivno = {(Aktivno ? 1 : 0)} " +
+            $"WHERE InstruktorId = {InstruktorId} AND KategorijaID = {KategorijaID}";
+
+        public List<IEntity> GetReaderList(SqlDataReader reader)
+        {
+            var list = new List<IEntity>();
+            while (reader.Read())
+            {
+                list.Add(new InstrKat
+                {
+                    InstruktorId = (int)reader["InstruktorId"],
+                    KategorijaID = (int)reader["KategorijaID"],
+                    DatumDodele = (DateTime)reader["DatumDodele"],
+                    Aktivno = (bool)reader["Aktivno"]
+                });
+            }
+            return list;
+        }
+
+        public IEntity GetReaderResult(SqlDataReader reader)
+        {
+            if (reader.Read())
+            {
+                return new InstrKat
+                {
+                    InstruktorId = (int)reader["InstruktorId"],
+                    KategorijaID = (int)reader["KategorijaID"],
+                    DatumDodele = (DateTime)reader["DatumDodele"],
+                    Aktivno = (bool)reader["Aktivno"]
+                };
+            }
+            return null;
+        }
+    }
+}
